bf986738e5d00ca1580735fdbc7db95956fcb9dd
Ashot Karapetian <AshotArtashesi@yandex.ru>
2023-12-12 10:28:12 +0300

    token function

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	t := mustToken()
+
+	//tgClient := telegram.New(t)
+
+	//fetcher := fetcher.New(tgClient)
+
+	//processor := processor.New(tgClient)
+
+	//consumer := consumer.Start(fetcher, processor)
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
 }
